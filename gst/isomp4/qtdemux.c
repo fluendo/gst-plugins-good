ac58fa86db6e624c4ef78424cd156712f1d61a21
Aleksandr Slobodeniuk <aslobodeniuk@fluendo.com>
2019-09-25 17:17:15 +0200

    qtdemux: extend max atom size
    We've found a file that has moov
    atom of size ~38 mb.

--- gst/isomp4/qtdemux.c
+++ gst/isomp4/qtdemux.c
@@ -85,7 +85,7 @@
 #endif
 
 /* max. size considered 'sane' for non-mdat atoms */
-#define QTDEMUX_MAX_ATOM_SIZE (25*1024*1024)
+#define QTDEMUX_MAX_ATOM_SIZE (100*1024*1024)
 
 /* if the sample index is larger than this, something is likely wrong */
 #define QTDEMUX_MAX_SAMPLE_INDEX_SIZE (50*1024*1024)
